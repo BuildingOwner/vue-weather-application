1399f02b3233bf976ebba22895e272f16d0579bd
BuildingOwner <106369213+BuildingOwner@users.noreply.github.com>
2023-02-26 19:35:12 +0900

    first commit

---
+++ src/components/HeaderDisplay.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header class="header">
+      <h2>Weather Application</h2>
+      <p>{{ username }}님 반갑습니다!!</p>
+    </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "weatherDisplay",
+};
+</script>
+
+<style scoped>
+.header {
+  background: linear-gradient(90deg, #fc5c7d, #6a82fb);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+}
+.header h2 {
+  color: #fff;
+}
+.header p {
+  font-size: 12px;
+  color: #fff;
+}
+</style>
